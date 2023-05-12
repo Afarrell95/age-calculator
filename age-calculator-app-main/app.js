399f482c768b10d9c8d8f4f7fe47b26be4ab72fd
Allison01~ <farrella95@gmail.com>
2023-05-12 14:17:19 -0400

    update

--- age-calculator-app-main/app.js
+++ age-calculator-app-main/app.js
@@ -1,7 +1,10 @@
-let dayInput = document.querySelector(".day-input");
-let monthInput = document.querySelector(".month-input");
-let yearInput = document.querySelector(".year-input");
+let dayInput = document.querySelector(".day-input").value;
+let monthInput = document.querySelector(".month-input").value;
+let yearInput = document.querySelector(".year-input").value;
 let button = document.querySelector(".circle");
 let yearDisplay = document.querySelector(".number-year");
 let monthDisplay = document.querySelector(".number-month");
 let dayDisplay = document.querySelector(".number-day");
+
+
+let date = new Date();
