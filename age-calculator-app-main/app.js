a7a61c97cbee75d4aec9f7447c95430d92ade174
Allison <farrella95@gmail.com>
2023-05-12 12:07:44 -0400

    query selectors set up

---
+++ age-calculator-app-main/app.js
@@ -0,0 +1,7 @@
+let dayInput = document.querySelector(".day-input");
+let monthInput = document.querySelector(".month-input");
+let yearInput = document.querySelector(".year-input");
+let button = document.querySelector(".circle");
+let yearDisplay = document.querySelector(".number-year");
+let monthDisplay = document.querySelector(".number-month");
+let dayDisplay = document.querySelector(".number-day");
